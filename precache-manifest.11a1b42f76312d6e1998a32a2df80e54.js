d5fd6794a6a8f52fc9b46155f6e785bf7d4e3ce8
Sarphu <tomonso.ejang@gmail.com>
2019-06-08 05:13:58 +0530

    portfolio in react

---
+++ precache-manifest.11a1b42f76312d6e1998a32a2df80e54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f366cfae6aa6946eb42b48c11f97078",
+    "url": "/index.html"
+  },
+  {
+    "revision": "696762225303ad130ed4",
+    "url": "/static/css/main.ce06a916.chunk.css"
+  },
+  {
+    "revision": "3c9096b2c4e046e2ee73",
+    "url": "/static/js/2.c6da80f0.chunk.js"
+  },
+  {
+    "revision": "696762225303ad130ed4",
+    "url": "/static/js/main.4662bca9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
